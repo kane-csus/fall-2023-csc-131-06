f912248566821811361ac3c32571579383d18545
Mansoor-Ali-0701 <mansoorali0701@gmail.com>
2023-10-08 22:37:04 -0700

    Added Mansoor Ali's info to the code

--- peer_printer.py
+++ peer_printer.py
@@ -19,8 +19,8 @@
 
 COURSE = ['CSC 131-06']
 SEMESTER = ['Fall 2023']
-
-
+ 
+ 
 class Person:
     '''
     This class is the Person class. It represents basic person info.
@@ -32,59 +32,39 @@ class Person:
         whether this person likes pineapple on pizza or not
     '''
     # Python constructor to initialize attributes (fields in Java)
-
+ 
     def __init__(self, name: str, likes_pineapple_pizza: bool):
         self.name = name
         self.likes_pineapple_pizza = likes_pineapple_pizza
-
+ 
     # Special Python method used to represent a class's objects as a string
     def __repr__(self):
         return self.name
-
-
+ 
+ 
 def main():
     # Person objects (alphabetical order by first name)
-<<<<<<< HEAD
-    aaron_goodlund = Person('Aaron Goodlund', True)
-=======
     aye_myat_noe_khin = Person('Aye Myat Noe Khin', True)
->>>>>>> 696db3d4bdd5973393f6410d491cabed7aad3922
     danica_galang = Person('Danica Galang', True)
     gary_kane = Person('Gary Kane', False)
     isabel_santoyogarcia = Person('Isabel Santoyo-Garcia', True)
     jomel_sotelo = Person('Jomel Sotelo', True)
     mansoor_ali = Person('Mansoor Ali', True)
     mia_brady = Person('Mia Brady', False)
-
-
+ 
     # List of people in our class (alphabetical order by first name)
-<<<<<<< HEAD
     peers = [aye_myat_noe_khin, danica_galang, gary_kane, isabel_santoyogarcia, jomel_sotelo, mansoor_ali, mia_brady]
-=======
-<<<<<<< HEAD
-    peers = [aaron_goodlund, danica_galang, gary_kane, isabel_santoyogarcia, jomel_sotelo, mia_brady]
-=======
-    peers = [aye_myat_noe_khin, danica_galang, gary_kane, isabel_santoyogarcia, jomel_sotelo, mia_brady]
->>>>>>> 696db3d4bdd5973393f6410d491cabed7aad3922
->>>>>>> 0c4038ab320b68e32b1568de335e968244ec91a7
-
+ 
     # Print out people in our class
     print("Welcome to learning Git in %s %s!" % (COURSE[0], SEMESTER[0]))
     print("Peers: %s" % peers)
-
+ 
     # Logic to see who likes pineapple pizza (alphabetical order by first name)
-<<<<<<< HEAD
-    if aaron_goodlund.likes_pineapple_pizza:
-        print("%s likes pineapple pizza" % aaron_goodlund.name)
-    else:
-        print("%s DOES NOT like pineapple pizza" %danica_galang.name)    
-=======
     if aye_myat_noe_khin.likes_pineapple_pizza:
         print("%s likes pineapple pizza" % aye_myat_noe_khin.name)
     else:
         print("%s DOES NOT like pineapple pizza" % aye_myat_noe_khin.name)
-
->>>>>>> 696db3d4bdd5973393f6410d491cabed7aad3922
+ 
     if danica_galang.likes_pineapple_pizza:
         print("%s likes pineapple pizza" % danica_galang.name)
     else:
@@ -93,29 +73,29 @@ def main():
         print("%s likes pineapple pizza" % gary_kane.name)
     else:
         print("%s DOES NOT like pineapple pizza" % gary_kane.name)
-
+ 
     if isabel_santoyogarcia.likes_pineapple_pizza:
         print("%s likes pineapple pizza" % isabel_santoyogarcia.name)
     else:
         print("%s DOES NOT like pineapple pizza" % isabel_santoyogarcia.name)
-
+ 
     if jomel_sotelo.likes_pineapple_pizza:
         print("%s likes pineapple pizza" % jomel_sotelo.name)
     else:
         print("%s DOES NOT like pineapple pizza" % jomel_sotelo.name)
-        
+ 
     if mansoor_ali.likes_pineapple_pizza:
         print("%s likes pineapple pizza" % mansoor_ali.name)
     else:
         print("%s DOES NOT like pineapple pizza" % mansoor_ali.name)
-        
+ 
     if mia_brady.likes_pineapple_pizza:
         print("%s likes pineapple pizza" % mia_brady.name)
     else:
         print("%s DOES NOT like pineapple pizza" % mia_brady.name)
-
-
-
-
+ 
+ 
+ 
+ 
 if __name__ == "__main__":
     main()
